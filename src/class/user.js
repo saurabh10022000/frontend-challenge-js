220023199cc70752c87ca7526339119b0713b725
saurabh10022000 <saurabhjain4916@gmail.com>
2024-03-06 10:20:01 -0800

    User Proceeding to next step even if  put 0 in field

--- src/class/user.js
+++ src/class/user.js
@@ -3,13 +3,15 @@ import convertNumber from '../utils/convertNumber';
 export class User {
   name = '';
 
-  income = 0;
+  income = NaN;
 
   education = '';
 
   constructor(params) {
+    console.log(params);
     this.name = params ? params.name ? params.name : '' : '';
-    this.income = params ? params.income ? convertNumber(params.income) : 0 : 0;
+    this.income = params && params.income !== undefined ? (params.income === '0' ? 0 : convertNumber(params.income)) : NaN;
+    // this.income = params ? params.income ? convertNumber(params.income) : 0 : 0;
     this.education = params ? params.education : '';
   }
 }
